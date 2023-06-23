b5abb9acdd6d5aefe39c9d9f82f673658d5d5f4d
TheCodeGeneral <camrpierce@gmail.com>
2023-06-23 00:18:51 -0500

    Changed layout

--- QuickHash/MainWindow.xaml.cs
+++ QuickHash/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
