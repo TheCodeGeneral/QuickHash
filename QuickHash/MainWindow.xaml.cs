11711127466e8e6ceb3bc8034c0cf967ae7c43ab
TheCodeGeneral <camrpierce@gmail.com>
2023-06-24 01:40:34 -0500

    Core functionality implemented

--- QuickHash/MainWindow.xaml.cs
+++ QuickHash/MainWindow.xaml.cs
@@ -28,58 +28,116 @@ namespace QuickHash
         {
             InitializeComponent();
         }
-        private void HashAll(string filePath)
+        // Compute hash of provided file and update the corresponding labels
+        private void HashAll(byte[] fileBuf)
         {
-            byte[] fileBuf = File.ReadAllBytes(filePath);
-
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            // Update hash results label
             Task.Run(() =>
             {
-                string sha1 = GetHash(fileBuf, SHA1.Create());
-                this.Dispatcher.Invoke(() => {
+                byte[] sha1Hash = SHA1.HashData(fileBuf);
+                string sha1 = ConvertHash(ref sha1Hash);
+                this.Dispatcher.Invoke(() => 
+                {
                     txtSha1.Text = sha1;
                 });
             });
             Task.Run(() =>
             {
-                string sha256 = GetHash(fileBuf, SHA256.Create());
-                this.Dispatcher.Invoke(() => {
+                byte[] sha256Hash = SHA256.HashData(fileBuf);
+                string sha256 = ConvertHash(ref sha256Hash);
+                this.Dispatcher.Invoke(() => 
+                {
                     txtSha256.Text = sha256;
                 });
             });
             Task.Run(() =>
             {
-                string sha384 = GetHash(fileBuf, SHA384.Create());
-                this.Dispatcher.Invoke(() => {
+                byte[] sha384Hash = SHA384.HashData(fileBuf);
+                string sha384 = ConvertHash(ref sha384Hash);
+                this.Dispatcher.Invoke(() => 
+                {
                     txtSha384.Text = sha384;
                 });
             });
             Task.Run(() =>
             {
-                string sha512 = GetHash(fileBuf, SHA512.Create());
-                this.Dispatcher.Invoke(() => {
+                byte[] sha512Hash = SHA512.HashData(fileBuf);
+                string sha512 = ConvertHash(ref sha512Hash);
+                this.Dispatcher.Invoke(() => 
+                {
                     txtSha512.Text = sha512;
                 });
             });
             Task.Run(() =>
             {
-                string md5 = GetHash(fileBuf, MD5.Create());
-                this.Dispatcher.Invoke(() => {
+                byte[] md5Hash = MD5.HashData(fileBuf);
+                string md5 = ConvertHash(ref md5Hash);
+                this.Dispatcher.Invoke(() => 
+                {
                     txtMd5.Text = md5;
                 });
             });
-
-
-
-
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
-        // Compute the file's hash
-        private string GetHash(byte[] fileBuf, HashAlgorithm hashAlgo)
+
+
+        // Compute hash of provided file and update the corresponding labels
+        private void HashAll(Stream fileStream)
+        {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Task.Run(() =>
+            {
+                byte[] sha1Hash = SHA1.HashData(fileStream);
+                string sha1 = ConvertHash(ref sha1Hash);
+                this.Dispatcher.Invoke(() =>
+                {
+                    txtSha1.Text = sha1;
+                });
+            });
+            Task.Run(() =>
+            {
+                byte[] sha256Hash = SHA256.HashData(fileStream);
+                string sha256 = ConvertHash(ref sha256Hash);
+                this.Dispatcher.Invoke(() =>
+                {
+                    txtSha256.Text = sha256;
+                });
+            });
+            Task.Run(() =>
+            {
+                byte[] sha384Hash = SHA384.HashData(fileStream);
+                string sha384 = ConvertHash(ref sha384Hash);
+                this.Dispatcher.Invoke(() =>
+                {
+                    txtSha384.Text = sha384;
+                });
+            });
+            Task.Run(() =>
+            {
+                byte[] sha512Hash = SHA512.HashData(fileStream);
+                string sha512 = ConvertHash(ref sha512Hash);
+                this.Dispatcher.Invoke(() =>
+                {
+                    txtSha512.Text = sha512;
+                });
+            });
+            Task.Run(() =>
+            {
+                byte[] md5Hash = MD5.HashData(fileStream);
+                string md5 = ConvertHash(ref md5Hash);
+                this.Dispatcher.Invoke(() =>
+                {
+                    txtMd5.Text = md5;
+                });
+            });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
+
+
+        // Convert the file's hash into a string
+        private string ConvertHash(ref byte[] rawHash)
         {
             string hash = "";
-            byte[] rawHash = hashAlgo.ComputeHash(fileBuf);
 
             foreach (byte b in rawHash)
             {
@@ -93,23 +151,67 @@ namespace QuickHash
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
-                HashAll(openFileDialog.FileName);
+                FileInfo fi = new(openFileDialog.FileName);
+                if (fi.Length < 2000000000)
+                { 
+                    HashAll(File.ReadAllBytes(openFileDialog.FileName));
+                }
+                else
+                {
+                    HashAll(File.OpenRead(openFileDialog.FileName));
+                }
             }
         }
 
         private void btnCopyAll_Click(object sender, RoutedEventArgs e)
         {
-
+            string allHashes = "Sha1:\t" + txtSha1.Text +
+                "\nSha256:\t" + txtSha256.Text +
+                "\nSha384:\t" + txtSha384.Text +
+                "\nSha512:\t" + txtSha512 +
+                "\nMd5:\t" + txtMd5.Text;
+            Clipboard.SetText(allHashes);
         }
 
         private void btnSearchOnline_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                System.Diagnostics.Process.Start("http://www.virustotal.com/gui/file/" + getSelectedHash());
+            }
+            catch (System.ComponentModel.Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (System.Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
         }
 
         private void btnCopy_Click(object sender, RoutedEventArgs e)
         {
-
+            Clipboard.SetText(getSelectedHash());
+        }
+        private string getSelectedHash()
+        {
+            switch (cmbHashSelection.SelectedIndex)
+            {
+                case 0:
+                    return txtSha1.Text;
+                case 1:
+                    return txtSha256.Text;
+                case 2:
+                    return txtSha384.Text;
+                case 3:
+                    return txtSha512.Text;
+                case 4:
+                    return txtMd5.Text;
+                default:
+                    MessageBox.Show("Error getting selected hash value: Index out of bounds");
+                    return "";
+            }
         }
 
         private void mnuEditPrefs_Click(object sender, RoutedEventArgs e)
